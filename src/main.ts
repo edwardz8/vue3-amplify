12420930f0cfe2fc43992613c814e4e99ea1f231
edwardz8 <edwardz8@yahoo.com>
2022-05-16 14:50:13 -0400

    init

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import AmplifyVue from '@aws-amplify/ui-vue';
+// import AmplifyVue from '@aws-amplify/ui-vue';
 // import { StoryblokVue, apiPlugin } from '@storyblok/vue';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(AmplifyVue);
 /* app.use(StoryblokVue, {
     accessToken: 'YOUR_ACCESS_TOKEN',
     use: [apiPlugin]
